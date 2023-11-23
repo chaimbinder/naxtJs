107fd1110f333dbad058e452c8b093725b2ed726
chaim binder <chaimbinder@gmail.com>
2023-11-23 15:29:38 +0200

    first commit

---
+++ my-app/app/componts/ProdctCrde.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import styles from './ProdctCrde.module.css'
+import AddTOCard from './AddTOCard'
+export default function ProdctCrde() {
+  return (
+    <div >
+       <AddTOCard ></AddTOCard>
+    </div>
+  )
+}
